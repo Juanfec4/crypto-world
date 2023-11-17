48a8de682911b70f0ab5b7db5aa0bfd84b023808
Juan <juanfelipecardenas4@gmail.com>
2023-11-17 17:56:15 -0500

    C'est  fini

--- src/router/pages/watchList/index.tsx
+++ src/router/pages/watchList/index.tsx
@@ -3,6 +3,7 @@ import { getAllWatchListed } from "../../../utils/helpers";
 import { fetchPageCoins } from "../../../utils/api";
 import { CoinData } from "../../../types/global";
 import CoinList from "../../../components/data-display/coinList";
+import { useNavigate } from "react-router-dom";
 
 const WatchlistPage: FC = () => {
   const [watchlistedCoinIds, setWatchlistedCoinIds] = useState<
@@ -11,12 +12,14 @@ const WatchlistPage: FC = () => {
 
   const [coins, setCoins] = useState<CoinData[] | undefined>(undefined);
 
+  const navigator = useNavigate();
+
   useEffect(() => {
     setWatchlistedCoinIds(getAllWatchListed());
   }, []);
 
   useEffect(() => {
-    if (watchlistedCoinIds) {
+    if (watchlistedCoinIds && watchlistedCoinIds.length > 0) {
       const idString = watchlistedCoinIds.toString();
       fetchPageCoins({
         ids: idString,
@@ -31,7 +34,24 @@ const WatchlistPage: FC = () => {
     }
   }, [watchlistedCoinIds]);
 
-  return <main>{coins ? <CoinList searchTerm="" coins={coins} /> : null}</main>;
+  return (
+    <main className="px-6">
+      {coins ? (
+        <CoinList searchTerm="" coins={coins} />
+      ) : (
+        <p className="mx-auto max-w-md lg:max-w-xl text-center mt-16 font-darker-grotesque  text-xl md:text-2xl lg:text-3xl px-6">
+          You do not have any watchlisted coins at the moment. Please select
+          some from the{" "}
+          <a
+            className="underline text-medium-purple cursor-pointer"
+            onClick={() => navigator("/coins")}
+          >
+            coin list.
+          </a>
+        </p>
+      )}
+    </main>
+  );
 };
 
 export default WatchlistPage;
